7688a41b742f4909aafc7a81d28018f453e4dd9c
newt0 <py_lambda@yahoo.co.jp>
2020-05-03 23:26:26 +0900

    fix: v-toolbars周りのスタイルを洗練

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,16 @@
 <template>
   <v-card color="grey lighten-4" flat tile fluid>
     <v-toolbar dense>
-      <v-toolbar-title>
+      <v-toolbar-title style="min-width: 50px">
         <v-img src="https://stayhomes.netlify.app/images/logo.png"></v-img>
       </v-toolbar-title>
 
-      <v-spacer></v-spacer>
-
-      <v-tabs align-with-title grow style="width: auto">
+      <v-tabs align-with-title grow style="min-width: auto">
         <v-tab>Tab 1</v-tab>
         <v-tab>Tab 2</v-tab>
         <v-tab>Tab 3</v-tab>
-        <v-tab>Tab 4</v-tab>
-        <v-tab>Tab 5</v-tab>
+        <!-- <v-tab>Tab 4</v-tab>
+        <v-tab>Tab 5</v-tab>-->
       </v-tabs>
     </v-toolbar>
   </v-card>
