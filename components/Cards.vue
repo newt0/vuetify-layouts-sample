83029c13e3ab546b632a3358f095c8cf99457f16
newt0 <py_lambda@yahoo.co.jp>
2020-05-04 02:32:28 +0900

    fix avatarと見出しにnuxt-linkを設定.MERY参考

--- components/Cards.vue
+++ components/Cards.vue
@@ -3,7 +3,39 @@
     <v-col lg6 xs12>
       <v-card class="mx-auto" outlined>
         <v-list-item three-line>
-          <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+          <nuxt-link to="/post">
+            <v-list-item-avatar
+              tile
+              size="80"
+              color="grey"
+            ></v-list-item-avatar>
+          </nuxt-link>
+          <v-list-item-content>
+            <div class="overline mb-4">カテゴリー</div>
+            <nuxt-link to="/post">
+              <v-list-item-title class="headline mb-1"
+                >記事の見出し</v-list-item-title
+              >
+            </nuxt-link>
+            <v-list-item-subtitle
+              >Greyhound divisely hello coldly
+              fonwderfully</v-list-item-subtitle
+            >
+            <v-list-item-subtitle>５分で読めます</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+    </v-col>
+    <v-col lg6 xs12>
+      <v-card class="mx-auto" outlined>
+        <v-list-item three-line>
+          <nuxt-link to="/blog">
+            <v-list-item-avatar
+              tile
+              size="80"
+              color="grey"
+            ></v-list-item-avatar>
+          </nuxt-link>
           <v-list-item-content>
             <div class="overline mb-4">カテゴリー</div>
             <nuxt-link to="/blog">
@@ -18,32 +50,6 @@
             <v-list-item-subtitle>５分で読めます</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
-        <!-- <v-card-actions>
-          <v-btn text>Button</v-btn>
-          <v-btn text>Button</v-btn>
-        </v-card-actions>-->
-      </v-card>
-    </v-col>
-    <v-col>
-      <v-card class="mx-auto" outlined>
-        <v-list-item three-line>
-          <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
-          <v-list-item-content>
-            <div class="overline mb-4">カテゴリー</div>
-            <v-list-item-title class="headline mb-1"
-              >記事の見出し</v-list-item-title
-            >
-            <v-list-item-subtitle
-              >Greyhound divisely hello coldly
-              fonwderfully</v-list-item-subtitle
-            >
-            <v-list-item-subtitle>５分で読めます</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-        <!-- <v-card-actions>
-          <v-btn text>Button</v-btn>
-          <v-btn text>Button</v-btn>
-        </v-card-actions>-->
       </v-card>
     </v-col>
   </v-row>
